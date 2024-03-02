c0dbc1eb6b616627ba3210ace8430d19ce5bdf65
Kyle Ricardo <kylericardo666@gmail.com>
2024-03-02 16:09:15 +0800

    feat: press any key to exit

--- src/util.rs
+++ src/util.rs
@@ -1,14 +1,4 @@
-use std::io;
-use std::io::{Read, Write};
-
 pub fn pause() {
-    let mut stdin = io::stdin();
-    let mut stdout = io::stdout();
-
-    // We want the cursor to stay at the end of the line, so we print without a newline and flush manually.
-    write!(stdout, "按回车键退出 Press enter to exit...").unwrap();
-    stdout.flush().unwrap();
-
-    // Read a single byte and discard
-    let _ = stdin.read(&mut [0u8]).unwrap();
+    use std::process::Command;
+    let _ = Command::new("cmd.exe").arg("/c").arg("pause").status();
 }
